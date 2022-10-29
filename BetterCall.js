fef8d384e4ed6480b42da13dcfa824adea31164a
prb404 <64775485+prb404@users.noreply.github.com>
2022-10-29 17:03:54 +0200

    BetterCalls

---
+++ BetterCall.js
@@ -0,0 +1,27 @@
+function loadjscssfileneoeg(filename, filetype){
+ if (filetype=="js"){ //if filename is a external JavaScript file
+  var fileJs=document.createElement('script')
+  fileJs.setAttribute("type","text/javascript")
+  fileJs.setAttribute("src", filename)
+ }
+ else if (filetype=="css"){ //if filename is an external CSS file
+  var fileref=document.createElement("link")
+  fileref.setAttribute("rel", "stylesheet")
+  fileref.setAttribute("type", "text/css")
+  fileref.setAttribute("href", filename)
+ }
+ if (typeof fileref!="undefined"){
+  document.getElementsByTagName("head")[0].appendChild(fileref)
+ }
+ else if (typeof fileJs!="undefined"){
+  document.getElementsByTagName("body")[0].appendChild(fileJs)
+ }
+}
+
+// external JS
+//loadjscssfileneoeg("https://url.com/.../customJavaScript.js", "js") //dynamically load and add this .js file
+loadjscssfileneoeg("https://cdn.logwork.com/widget/countdown.js", "js")
+
+// external CSS
+//loadjscssfileneoeg("https://url.com/.../customStyleSheet.css", "css") ////dynamically load and add this .css file
+loadjscssfileneoeg("https://prb404.github.io/NEO/neo/customStyleSheetNEOEG.css", "css")
